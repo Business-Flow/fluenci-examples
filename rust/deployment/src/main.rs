5bd28e70f5500e110327f33d86b5cfa2aa98dfec
David Falkner <ardave@gmail.com>
2024-05-31 22:46:39 -0600

    Added delay to test portal's automatic status refreshing.

--- rust/deployment/src/main.rs
+++ rust/deployment/src/main.rs
@@ -1,3 +1,14 @@
+use std::thread;
+use std::time::Duration;
+
 fn main() {
     println!("Hello from the Deployment Script!");
+    
+    // Arbitrary to test the portal's automatic status refreshing.
+    for i in 1..=15 {
+        println!("Iteration {}", i);
+
+        thread::sleep(Duration::from_secs(1));
+    }
+    println!("Deployment Script Completed.");
 }
