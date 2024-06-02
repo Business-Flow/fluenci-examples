87f2b3e69eed8d8f5af65a75f95b495711c93006
David Falkner <ardave@gmail.com>
2024-06-01 23:57:04 -0600

    Updated examples a bit

--- csharp/deployment/Program.cs
+++ csharp/deployment/Program.cs
@@ -1,9 +1,20 @@
-﻿Console.WriteLine("Hello, from the C# Deployment Project!");
-
-var sw = System.Diagnostics.Stopwatch.StartNew();
-
-while (sw.Elapsed < TimeSpan.FromMinutes(1))
+﻿using System;
+using System.Linq;
+using System.Threading;
+					
+public class Program
 {
-    Console.WriteLine($"Sleeping at {DateTime.UtcNow} ...");
-    Thread.Sleep(TimeSpan.FromSeconds(5));
+	public static void Main()
+	{
+		Console.WriteLine("Starting C# pipeline.");
+		
+		// Arbitrary delay to test the portal's automatic status refreshing.
+		foreach (var i in Enumerable.Range(1, 15).Reverse()) 
+		{
+			Console.WriteLine($"Sleeping for {i} seconds ...");
+			Thread.Sleep(TimeSpan.FromSeconds(1));
+		}
+		
+		Console.WriteLine("C# pipeline completed!");
+	}
 }
--- rust/deployment/src/main.rs
+++ rust/deployment/src/main.rs
@@ -4,9 +4,9 @@ use std::time::Duration;
 fn main() {
     println!("Hello from the Deployment Script!");
     
-    // Arbitrary to test the portal's automatic status refreshing.
-    for i in 1..=15 {
-        println!("Iteration {}", i);
+    // Arbitrary delay to test the portal's automatic status refreshing.
+    for i in 15..=1 {
+        println!("Pausing for {} seconds...", i);
 
         thread::sleep(Duration::from_secs(1));
     }
