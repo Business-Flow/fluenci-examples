f228558848eb8110572030481687e4e8db6f5d42
David Falkner <ardave@gmail.com>
2024-07-03 15:02:19 -0600

    mimetype="text/html"

--- python/function_app.py
+++ python/function_app.py
@@ -12,5 +12,6 @@ def hello_world(req: func.HttpRequest) -> func.HttpResponse:
     return func.HttpResponse(
             """<h1>Hello from a <a href="https://fluenci.co" target="_blank">FluenCI.co</a>-deployed Python app!</h1>
                <h3>(Click <a href="https://github.com/dave-biz/fluenci-examples/blob/main/python/deployment.py" target="_blank">here</a> to view the pipeline that deployed me.)</h3>""",
-            status_code=200
+            status_code=200,
+            mimetype="text/html"
     )
