947a22efa6e14fc1a04d5f41e266c28d981dc9e6
David Falkner <ardave@gmail.com>
2024-07-02 22:41:40 -0600

    "final" html content

--- python/function_app.py
+++ python/function_app.py
@@ -8,6 +8,7 @@ def hello_world(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     return func.HttpResponse(
-            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+            """<h1>Hello from a <a href="https://fluenci.co" target="_blank">FluenCI.co</a>-deployed Go app!</h1>
+               <h3>(Click <a href="https://github.com/dave-biz/fluenci-examples/blob/main/go/deployment/main.go" target="_blank">here</a> to view the pipeline that deployed me.)</h3>""",
             status_code=200
     )
