19d15b9a47d1425e84150c82315718b66319cbae
David Falkner <ardave@gmail.com>
2024-07-12 15:45:08 -0600

    updated C# deployment

---
+++ csharp/csharp_project/hello_world.cs
@@ -0,0 +1,33 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace csharp_project;
+
+    public class hello_world
+    {
+        private readonly ILogger _logger;
+
+        public hello_world(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<hello_world>();
+        }
+
+        [Function("hello_world")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/html; charset=utf-8");
+
+            response.WriteString("""
+               <h1>Hello from a <a href="https://fluenci.co" target="_blank">FluenCI.co</a>-deployed C# app!</h1>
+               <h3>(Click <a href="https://github.com/dave-biz/fluenci-examples/blob/main/csharp/deployment/Program.cs" target="_blank">here</a> to view the pipeline that deployed me.)</h3>
+            """);
+
+            return response;
+        }
+    }
+
--- csharp/deployment/Program.cs
+++ csharp/deployment/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 					
@@ -7,14 +8,57 @@ public class Program
 	public static void Main()
 	{
 		Console.WriteLine("Starting C# pipeline.");
-		
-		// Arbitrary delay to test the portal's automatic status refreshing.
-		foreach (var i in Enumerable.Range(1, 15).Reverse()) 
+		var azureClientId = GetEnvironmentVariableOrFail("AZURE_CLIENT_ID");
+		var azureSecret = GetEnvironmentVariableOrFail("AZURE_SECRET");
+		var azureTenant= GetEnvironmentVariableOrFail("AZURE_TENANT");
+		var azureSubscriptionId = GetEnvironmentVariableOrFail("AZURE_SUBSCRIPTION_ID");
+
+		var projectCodeDirectory = "csharp/csharp_project";
+		Console.WriteLine($"Changing to directory: {projectCodeDirectory}");
+		Directory.SetCurrentDirectory(projectCodeDirectory);
+
+		Console.WriteLine($"Current directory is: {Directory.GetCurrentDirectory()}");
+
+		Console.WriteLine("Publishing Function App ...");
+		RunCommand("func", "azure functionapp publish fluenci-csharp-demo --verbose");
+	}
+
+	public static string GetEnvironmentVariableOrFail(string name)
+	{
+		var value = Environment.GetEnvironmentVariable(name);
+
+		if (value is null)
+			throw new Exception($"Missing environment variable: '{name}'.");
+
+		return value;
+	}
+
+	public static void RunCommand(string command, string arguments)
+	{
+		var process = new Process 
 		{
-			Console.WriteLine($"Sleeping for {i} seconds ...");
-			Thread.Sleep(TimeSpan.FromSeconds(1));
+			StartInfo = new ProcessStartInfo
+			{
+				FileName = command,
+				Arguments = arguments,
+				RedirectStandardOutput = true,
+				RedirectStandardError = true,
+				UseShellExecute = false,
+				CreateNoWindow = true
+			}
+		};	
+
+		process.Start();
+
+		string result = process.StandardOutput.ReadToEnd();
+		string error = process.StandardError.ReadToEnd();
+
+		process.WaitForExit();
+
+		Console.WriteLine("Output: \n" + result);
+		if (!string.IsNullOrWhiteSpace(error))
+		{
+			Console.WriteLine("Error: \n" + error);
 		}
-		
-		Console.WriteLine("C# pipeline completed!");
 	}
 }
